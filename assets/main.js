ac41033efc42f030eba4a10f36c86fda35a0818b
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-21 12:36:09 +0200

    add modifies to js doc

--- assets/main.js
+++ assets/main.js
@@ -6,24 +6,42 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         console.log(cards);
         let cardElement = ''
         cards.forEach(card => {
-            const {title, url} = card
+            const { title, url } = card
             console.log(card);
-            
             const markup = `
             <div class="col-12 col-md-5 col-lg-3 m-3 photo">
                 <div class="image">
+                    <img id="card-img" src="${url}" alt="">    
                     <img class="pin" src="./img/pin.svg" alt="">
-                    <img class="card-img" type="button" src="${url}" alt="">    
                 </div>
                 <div class="description">
-                    ${title}
+                    ${title.charAt(0).toUpperCase() + title.slice(1).toLowerCase()}
                 </div>
                 </div>
             `
             cardElement += markup
             console.log(cardElement);
+
+            rowEl.innerHTML = cardElement
+
             
         })
+        
+        let imgEl = document.getElementById('card-img')
+        console.log(imgEl);
+ 
+        const overlayEl = document.getElementById('overlay')
+        const buttonEl = document.querySelector('button')
 
-        rowEl.innerHTML = cardElement
-    }) .catch(error => console.error(error)) 
+
+        /* imgEl.addEventListener("click", () => {
+            overlayEl.classList.remove('d-none')
+        });
+
+        buttonEl, overlayEl.addEventListener('click', () => {
+            overlayEl.classList.add('d-none')
+        }) */
+
+
+    }).catch(error => console.error(error))
+
